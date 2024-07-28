fd9d5ad65b38d1f8ad22d9a7f8513bc024673f10
padil <fadhilgani2@gmail.com>
2024-07-28 21:20:33 +0700

    add footer section

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,13 @@
 import HeroSection from "@/components/HeroSection";
 import CarouselSection from "@/components/Carousel";
+import Footer from "@/components/Footer";
 
 export default function Home() {
     return (
         <div className="overflow-hidden">
             <HeroSection/>
             <CarouselSection/>
+            <Footer/>
         </div>
     );
 }
