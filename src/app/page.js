990b7bcf6de9e532616f46c8c9f2dbc686c06d95
padiil <fadhilgani2@gmail.com>
2024-07-29 13:26:08 +0700

    revisi footer(export to layout, change js to jsx, change color bg-primary)

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,11 @@
 import HeroSection from "@/components/HeroSection";
 import CarouselSection from "@/components/Carousel";
-import Footer from "@/components/Footer";
 
 export default function Home() {
     return (
         <div className="overflow-hidden">
             <HeroSection/>
             <CarouselSection/>
-            <Footer/>
         </div>
     );
 }
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import Logo from "../../public/assets/logo.svg";
 
 export default function Footer() {
     return (
-        <footer className="bg-[#222831] ">
+        <footer className="bg-primary ">
             <div className="mx-auto max-w-[1440px] space-y-8 px-4 py-16 sm:px-6 lg:space-y-16 lg:px-8">
                 <div className="grid grid-cols-1 gap-8 lg:grid-cols-3">
 
