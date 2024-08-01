5b02e6969f3ec4ccd094d02a0e38e510d1e98ed9
kudith <ginanjar.aditya15@gmail.com>
2024-08-01 14:03:00 +0700

    save changes before merging

--- src/app/destination/page.js
+++ src/app/destination/page.js
@@ -1,7 +1,24 @@
+"use client"
+import {Swiper, SwiperSlide} from "swiper/react";
+import 'swiper/css';
+
+const data = [
+    {img: "/public/assets/bromo3.jpg", alt:"Gunung Bromo"}
+]
 export default function DestinationPage() {
     return (
         <div className="text-primary font-bold">
-            <h1>Welcome to Destination in indonesia</h1>
+            <Swiper
+                spaceBetween={50}
+                slidesPerView={3}
+                onSlideChange={() => console.log('slide change')}
+                onSwiper={(swiper) => console.log(swiper)}
+            >
+                <SwiperSlide>Slide 1</SwiperSlide>
+                <SwiperSlide>Slide 2</SwiperSlide>
+                <SwiperSlide>Slide 3</SwiperSlide>
+                <SwiperSlide>Slide 4</SwiperSlide>
+            </Swiper>
         </div>
     )
 }
